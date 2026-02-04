ae0ce96aa82d5dc444a7e3a3a9bf15298cf4e470
Peter Hedlund <support@pbh.dev>
2026-02-03 19:03:55 -0800

    Remove unused code

--- CloudDeck/Networking/Router.swift
+++ CloudDeck/Networking/Router.swift
@@ -25,21 +25,21 @@ enum Method: String {
 }
 
 enum Router {
-        case boards
-        case board(id: Int)
-        case stacks(boardId: Int)
-        case cards(stackId: Int)
-        case card(id: Int)
-
+    case boards
+    case board(id: Int)
+    case stacks(boardId: Int)
+    case cards(stackId: Int)
+    case card(id: Int)
+    
     private var method: Method {
         switch self {
         case .boards:
             return .get
         case .board, .stacks, .cards, .card:
-              return .get
+            return .get
         }
     }
-
+    
     private var path: String {
         switch self {
         case .boards:
@@ -54,19 +54,19 @@ enum Router {
             "/cards/\(id)"
         }
     }
-
+    
     private var basicAuthHeader: String {
         return ValetManager.shared.basicAuthHeader
     }
-
+    
     func urlRequest() throws -> URLRequest {
         @AppStorage(Constants.Settings.server) var server: String = ""
-
+        
         let baseURLString = "\(server)/index.php/apps/deck/api/v1.1"
         guard let url = URL(string: baseURLString) else {
             throw URLError(.badURL)
         }
-
+        
         var urlRequest = URLRequest(url: url.appendingPathComponent(path))
         urlRequest.httpMethod = method.uppercased
         urlRequest.cachePolicy = .reloadIgnoringLocalCacheData
@@ -75,7 +75,7 @@ enum Router {
         urlRequest.setValue("true", forHTTPHeaderField: "OCS-APIRequest")
         // urlRequest.setValue("Wed, 04 Feb 2026 00:17:06 GMT", forHTTPHeaderField: "If-Modified-Since")
         urlRequest.setValue("application/json", forHTTPHeaderField: "Accept")
-
+        
         switch self {
         case .boards:
             break
@@ -88,64 +88,7 @@ enum Router {
         case .card(let id):
             break
         }
-
-            return urlRequest
+        
+        return urlRequest
     }
-//    func asURLRequest(
-//        username: String,
-//        appPassword: String,
-//        etag: String? = nil
-//    ) throws -> URLRequest {
-//
-//        var components = URLComponents(
-//            url: baseURL.appendingPathComponent(path),
-//            resolvingAgainstBaseURL: false
-//        )
-//        components?.queryItems = queryItems
-//
-//        guard let url = components?.url else {
-//            throw URLError(.badURL)
-//        }
-//
-//        var request = URLRequest(url: url)
-//        request.httpMethod = method.rawValue.uppercased()
-//        request.httpBody = body
-//
-//        request.setValue("application/json", forHTTPHeaderField: "Accept")
-//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-//
-//        let auth = "\(username):\(appPassword)"
-//        let encoded = Data(auth.utf8).base64EncodedString()
-//        request.setValue("Basic \(encoded)", forHTTPHeaderField: "Authorization")
-//
-//        if let etag {
-//            request.setValue(etag, forHTTPHeaderField: "If-None-Match")
-//        }
-//
-//        return request
-//    }
-
 }
-
-//extension DeckRouter {
-//
-//    static func paginatedCards(
-//        baseURL: URL,
-//        stackId: Int,
-//        limit: Int,
-//        offset: Int
-//    ) -> DeckRouter {
-//
-//        DeckRouter(
-//            baseURL: baseURL,
-//            endpoint: .cards(stackId: stackId),
-//            method: .get,
-//            queryItems: [
-//                .init(name: "limit", value: "\(limit)"),
-//                .init(name: "offset", value: "\(offset)")
-//            ],
-//            body: nil
-//        )
-//    }
-//}
-
